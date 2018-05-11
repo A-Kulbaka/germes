d7a6b70ed74adbf889aec97ea870c6fc59d8b5e2
A-Kulbaka <myprivatemailonlyforwork@gmail.com>
2018-05-11 18:04:28 +0300

    add Runner.java

---
+++ src/main/java/com/mycompany/germes/application/model/Runner.java
@@ -0,0 +1,30 @@
+package com.mycompany.germes.application.model;
+
+import java.lang.reflect.Field;
+
+public class Runner {
+
+    private String s = "mister";
+    public Runner() {
+        if (System.getSecurityManager() == null){
+            System.setSecurityManager(new SecurityManager());
+        }
+    }
+
+    public void sayHi(){
+        System.out.println("hi");
+    }
+
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.sayHi();
+        Field[] declaredFields = runner.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println(declaredField);
+            declaredField.setAccessible(true);
+        }
+
+        runner.sayHi();
+
+    }
+}
